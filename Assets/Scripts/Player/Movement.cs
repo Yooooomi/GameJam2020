6568764de50401fdf863f7bc2137bde50d1c915e
Noe Rivals <noe.rivals@gmail.com>
2020-02-01 18:47:58 +0100

    [+]: add mortar

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -19,7 +19,7 @@ public class Movement : MonoBehaviour
     private Vector2 desired;
     public Inputs input;
 
-    public Animator animator;
+    public Animator animator = null;
 
     private Direction currentDirection;
 
@@ -41,8 +41,11 @@ public class Movement : MonoBehaviour
         {
             return;
         }
-        animator.SetBool("left", dir == Direction.LEFT);
-        animator.SetBool("right", dir == Direction.RIGHT);
+        if (animator != null)
+        {
+            animator.SetBool("left", dir == Direction.LEFT);
+            animator.SetBool("right", dir == Direction.RIGHT);
+        }
         currentDirection = dir;
     }
 
